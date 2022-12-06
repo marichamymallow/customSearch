aad4c446b5132396c7e79a7a7b2e544186f37346
Marichamy Murugan <marichamy@mallow-tech.com>
2022-12-06 15:44:57 +0530

    concurrent update and list limit by value update

--- search.js
+++ search.js
@@ -1,7 +1,7 @@
 ;(function ($) {
     $.fn.customSearch = function (options) {
         var defaults = $.extend({
-            loading: function () {
+           loading: function () {
                 return new Promise((resolve, reject) => {
                     resolve("default resolve");
                 });
@@ -9,6 +9,10 @@
             selectElem: $(this),
             hideCheckBox: false,
             closeOnCheck: false,
+            multiselect: true,
+            maxListValElem: null,
+            showDeselectOnly: false,
+            deSelectTemplate: "",
             openMenu: function (elem, isPlugin) {
                 if (isPlugin) {
                     if(!$(elem).hasClass("opened")) {
@@ -35,6 +39,7 @@
             targetId: "",
             opentType: 'click',
             search: true,
+            hideClearIcon: false,
             popupStyles: '',
             searchStyle: '',
             optionLabelStyle: '',
@@ -47,8 +52,10 @@
             badgeColors: '',
             cancelBtnstyle: '',
             combineSelectorsTemplate: '',
+            hideCombineSelectors: false,
             onItemCheck: function () { },
             defaultCheck: undefined,
+            conCurrentSelect: null,
         }, options)
 
         return $(defaults.selectElem).each(function () {
@@ -72,7 +79,7 @@
                             </div>
                             <button class="deselect-btn">None</button>
                         </div>
-                        <div id="select-list" style="margin-top: 15px"></div>
+                        <div id="select-list" data-select-id="${$(elem).attr("id")}" style="margin-top: 15px"></div>
                     </div>
                     </div>`;
                 $(searchArea).insertAfter(elem);
@@ -82,18 +89,38 @@
                 var closeEvent = 'click';
                 var dropDownList = [];
                 var selectedValues = [];
+                var selectedTexts = [];
 
                 if(defaults.clickBtnTemplate != '') {
                     $(parent).find('#drop-btn').replaceWith(defaults.clickBtnTemplate);
                 }
 
-                if(defaults.searchTemplate != "") {
+                if($(elem).attr("initialValue") && $(elem).attr("initialValue").length>2) {
+                    var val = $(elem).attr("initialValue");
+                    $(elem).val(val);
+                    $(parent).find('#drop-btn').text(val);
+                }
+
+                if(defaults.searchTemplate != "" && defaults.search) {
                     $(parent).find("#select-search").replaceWith(defaults.searchTemplate);
                 }
 
-                if(defaults.combineSelectorsTemplate != "") {
+                if(defaults.combineSelectorsTemplate != "" && !defaults.hideCombineSelectors) {
                     $(parent).find(".selected-toggle").replaceWith(defaults.combineSelectorsTemplate);
                 }
+
+                if(defaults.showDeselectOnly) {
+                    $(parent).find(".select-all-btn").remove();
+                }
+
+                if(defaults.hideCombineSelectors) {
+                    $(parent).find(".selected-toggle").remove();
+                }
+
+                if(!defaults.search) {
+                    $(parent).find("#select-search").remove();
+                }
+
                 const listElem = $(parent).find('#select-list'),
                     searchElem = $(parent).find("#select-search"),
                     selectAll = $(parent).find('.select-all-btn'),
@@ -110,10 +137,6 @@
                 }
 
                 $(parent).find('#select-list').prepend(defaults.loadingTemplate);
-
-                if (defaults.targetId == "") {
-                    defaults.targetId = $(parent).find('#selected-ids');
-                }
 
                 if (defaults.listType != "checkbox") {
                     $(parent).find('.selected-toggle').remove();
@@ -151,17 +174,17 @@
                                 dropDownList.push(obj);
                             }
                         })
-
+                        let initialValue = $(elem).attr("initialValue");
                         // Looping list Items
                         $.each(dropDownList, function (key, item) {
                             let id = Math.floor((Math.random() * 100000) + 1);
-                            var elem = `<div class='list-option ${defaults.listStyle}' id="${id}">
-                                    <input type="${defaults.listType}" class="${defaults.checkboxStyle}" style=${defaults.hideCheckBox?inputStyle:""} name="${defaults.listType}" value="${item.val}"/>
+                            var elem = `<div class='list-option ${defaults.listStyle} ${initialValue?initialValue == item.val?"selected":"":""}' id="${id}" data-list-id="${item.val}">
+                                    <input type="${defaults.listType}" class="${defaults.checkboxStyle} ${initialValue?initialValue == item.val?"checked":"":""}" style='${defaults.hideCheckBox?inputStyle:""}' name="${defaults.listType}" value="${item.val}" checked='${initialValue?initialValue == item.val?true:false:false}'/>
                                     <span class="option-name ${defaults.optionLabelStyle}">${item.name}</span></div>`;
 
                             if(defaults.listTemplate != '') {
                                 var elem = $.parseHTML(defaults.listTemplate);
-                                $(elem).attr("id", id);
+                                $(elem).attr({"id":id, "data-list-id": item.val});
                                 $(elem).find('input').attr({type: defaults.listType, name: defaults.listType, value: item.val});
                                 $(elem).find('.option-name').text(item.name);
                                 $(listElem).append(elem);
@@ -187,9 +210,12 @@
                         if(defaults.defaultCheck) {
                             $.each(defaults.defaultCheck, function(key, item){
                                 selectedValues.push(item);
+                                selectedTexts.push(item);
                                 $(parent).find(`input[value=${item}]`).attr("checked", true).addClass("checked");
                                 $(parent).find(`input[value=${item}]`).closest('.list-option').addClass("selected");
                             })
+                            $(elem).val(selectedValues);
+                            defaults.onItemCheck.call(null, selectedValues, selectedTexts);
                         }
 
 
@@ -198,28 +224,54 @@
                             $(parent).find('.list-option').removeClass('focused');
                             $(this).addClass('focused');
                             var value = $(this).find('input').val();
+                            var text = $(this).find('.option-name').text();
                             if (defaults.listType == 'radio') {
                                 selectedValues = [];
+                                selectedTexts = [];
                             }
-                            if ($(this).find('input')[0].checked == false) {
+                            if(defaults.multiselect) {
+                                if ($(this).find('input')[0].checked == false) {
+                                    $(this).addClass('selected');
+                                    $(this).find('input').attr('checked', true);
+                                    $(this).find('input').addClass('checked');
+                                    selectedValues.push(value);
+                                    selectedTexts.push(text);
+                                } else {
+                                    selectedValues = $.grep(selectedValues, function (n) {
+                                        return n != value;
+                                    })
+                                    selectedTexts = $.grep(selectedTexts, function (n) {
+                                        return n != text;
+                                    })
+                                    $(this).removeClass('selected');
+                                    $(this).find('input').removeAttr('checked');
+                                    $(this).find('input').removeClass('checked');
+                                }
+                            }
+                            else {
+                                var listId = $(this).attr('data-list-id');
+                                $(parent).find('.list-option.selected input').removeAttr('checked').removeClass('checked');
+                                $(parent).find('.list-option.selected').removeClass('selected');
+                                selectedValues = [];
+                                selectedTexts = [];
                                 $(this).addClass('selected');
                                 $(this).find('input').attr('checked', true);
                                 $(this).find('input').addClass('checked');
+                                selectedTexts.push(text);
                                 selectedValues.push(value);
-                            } else {
-                                selectedValues = $.grep(selectedValues, function (n) {
-                                    return n != value;
-                                })
-                                $(this).removeClass('selected');
-                                $(this).find('input').removeAttr('checked');
-                                $(this).find('input').removeClass('checked');
+                                if(defaults.conCurrentSelect) {
+                                    $("[data-select-id="+defaults.conCurrentSelect+"]").find('.list-option').each(function(){
+                                        if($(this).attr('data-list-id') == listId) {
+                                            $(this).find('input').click();
+                                        }
+                                    })
+                                }
                             }
 
-                            //Changing select tag value for AJAX call
-                            $(elem).val(selectedValues);
-
                             if (!submitBtn) {
-                                defaults.onItemCheck.call(null, selectedValues);
+                                //Changing select tag value for AJAX call
+                                $(elem).val(selectedValues);
+                                defaults.onItemCheck.call(null, selectedValues, selectedTexts);
                                 if (defaults.closeOnCheck) {
                                     defaults.closeMenu(parent, true);
                                 }
@@ -231,7 +283,8 @@
 
                         //submitBtn Click
                         $(parent).find('#submit').on('click', function () {
-                            defaults.onItemCheck.call(null, selectedValues);
+                            $(elem).val(selectedValues);
+                            defaults.onItemCheck.call(null, selectedValues, selectedTexts);
                             defaults.closeMenu(parent, true);
                         })
 
@@ -242,8 +295,61 @@
                     }
                 }
 
+                function stringToFloat(str) {
+                    if(str) {
+                        var tempVal = str.split(":");
+                        if(tempVal.length>0) {
+                            var tempString = tempVal[0] + "." + tempVal[1];
+                            return parseFloat(tempString);
+                        }
+                    }
+                }
+
+
+                function removeMinimum(max) {
+                    $(parent).find('.list-option').each(function() {
+                        var listValue = stringToFloat($(this).find('.option-name').text());
+                        if(listValue > max ) {
+                            $(this).remove();
+                        }
+                    })
+                }
+
                 //open lists
                 $(dropELem).on(defaults.opentType, function () {
+                    // checking actual selected value
+                    var existingValues = $(elem).val();
+                    if(defaults.maxListValElem) {
+                        if(parseInt($(defaults.maxListValElem).val()) >= 0) {
+                            var maxListValue = stringToFloat($(defaults.maxListValElem).val());
+                            removeMinimum(maxListValue);
+                        }else {
+                            return false;
+                        }
+                    }
+                    if(typeof existingValues == 'string') {
+                        existingValues = [];
+                        existingValues.push($(elem).val())
+                    }
+                    $(parent).find('.list-option').each(function() {
+                        var listId = $(this).attr('data-list-id');
+                        if(existingValues && existingValues.length>0 && $.inArray(listId, existingValues) != -1) {
+                            if($.inArray(listId, selectedValues) == -1) {
+                                selectedValues.push(listId);
+                            }
+                            $(this).addClass('selected');
+                            $(this).find('input').attr('checked', true);
+                            $(this).find('input').addClass('checked');
+                        }
+                        else {
+                            selectedValues = $.grep(selectedValues, function (n) {
+                                return n != listId;
+                            })
+                            $(this).removeClass('selected');
+                            $(this).find('input').removeAttr('checked');
+                            $(this).find('input').removeClass('checked');
+                        }
+                    })
                     defaults.openMenu(parent, true);
                     $(searchElem).val("");
                     $(searchElem).focus();
@@ -329,9 +435,15 @@
                     $(defaults.targetId).empty();
                     if (selectedValues.length > 0) {
                         $.each(selectedValues, function (key, item) {
-                            var badge = `<div class="selected-badge"><span>${item}</span><a class='close-icon' data-href="${item}">X</a></div>`;
-                            $(defaults.targetId).append(badge);
+                            if(!defaults.hideClearIcon) {
+                                var badge = `<div class="selected-badge"><span>${item}</span><a class='close-icon' data-href="${item}">X</a></div>`;
+                                $(defaults.targetId).append(badge);
+                            } else {
+                                $(parent).find(defaults.targetId).text(selectedTexts[key]);
+                            }
                         })
+                    } else {
+                        $(parent).find(defaults.targetId).text("");
                     }
 
                     $(parent).find('.close-icon').on('click', function () {
@@ -340,10 +452,16 @@
                             selectedValues = $.grep(selectedValues, function (n) {
                                 return n != val;
                             })
+                            selectedTexts = $.grep(selectedTexts, function (n) {
+                                return n != val;
+                            })
                             $(parent).find('input[value=' + val + ']').prop("checked", false);
                             $(this).parent().remove();
                         }
                     })
+                    if(!defaults.multiselect) {
+                        defaults.closeMenu(parent, true);
+                    }
                 }
 
                 // Check All
@@ -351,10 +469,12 @@
                     $(parent).find('.list-option input').each(function () {
                         if(!$(this).closest('.list-option').attr('data-hide') && !$(this).attr('checked')) {
                             var value = $(this).val();
+                            var text = $(this).closest('.list-option').find('.option-name').text();
                             $(this).closest('.list-option').removeClass('selected');
                             $(this).closest('.list-option').addClass('selected');
                             $(this).attr('checked', true);
                             selectedValues.push(value);
+                            selectedTexts.push(text);
                         }
                     })
 
@@ -367,21 +487,39 @@
 
                 // Decheck All
                 $(deCheckElem).on('click', function () {
-                    $(parent).find('.list-option input').each(function () {
-                        if(!$(this).closest('.list-option').attr('data-hide')) {
-                            var val = $(this).val();
-                            selectedValues = $.grep(selectedValues, function (n) {
-                                return n != val;
-                            })
-                            $(this).removeAttr('checked');
-                            $(this).removeClass("checked");
-                            $(this).closest('.list-option').removeClass('selected');
-                            $(selectAll).prop("checked", false);
+                    if(!defaults.multiselect) {
+                        var elem = $(parent).find('.list-option.selected');
+                        if(elem.length > 0) {
+                            $(elem).find('input').removeClass('checked').removeAttr('checked');
+                            $(elem).removeClass('selected');
+                            selectedValues = [];
+                            selectedTexts = [];
                         }
-                    })
+                    }else {
+                        $(parent).find('.list-option input').each(function () {
+                            if(!$(this).closest('.list-option').attr('data-hide')) {
+                                var val = $(this).val();
+                                var text = $(this).closest('.list-option').find('.option-name').text();
+                                selectedValues = $.grep(selectedValues, function (n) {
+                                    return n != val;
+                                })
+                                selectedTexts = $.grep(selectedTexts, function (n) {
+                                    return n != text;
+                                })
+                                $(this).removeAttr('checked');
+                                $(this).removeClass("checked");
+                                $(this).closest('.list-option').removeClass('selected');
+                                $(selectAll).prop("checked", false);
+                            }
+                        })
+                    }
 
-                    //Changing select tag value for AJAX call
-                    $(elem).val(selectedValues);
+                    if (!submitBtn) {
+                        //Changing select tag value for AJAX call
+                        $(elem).val(selectedValues);
+
+                        defaults.onItemCheck.call(null, selectedValues, selectedTexts);
+                    }
 
                     if(!defaults.hideSelected)
                         showSelected();
